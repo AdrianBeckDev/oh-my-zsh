9a7c16453217135f33c430696581950208b4bdaa
ornicar <thibault.duplessis@gmail.com>
2010-12-25 00:21:50 +0100

    Add git alias, remove keybinding in completion script

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -15,6 +15,7 @@ alias gba='git branch -a'
 alias gcount='git shortlog -sn'
 alias gcp='git cherry-pick'
 alias glg='git log --stat --max-count=5'
+alias gco='git checkout'
 
 # Git and svn mix
 alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
