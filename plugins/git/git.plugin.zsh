b0b369c2dc5daba466b7f7a6db25a0d70ae769b0
ornicar <thibault.duplessis@gmail.com>
2010-12-25 22:27:45 +0100

    Add alias for "git mergetool"

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -16,6 +16,7 @@ alias gcount='git shortlog -sn'
 alias gcp='git cherry-pick'
 alias glg='git log --stat --max-count=5'
 alias gco='git checkout'
+alias gmt='git mergetool'
 
 # Git and svn mix
 alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
