1987f02949bc8e814edcd422d75a00a516e882b2
ornicar <thibault.duplessis@gmail.com>
2011-01-18 11:12:52 -0800

    Add zsh aliases to switch hardware and start the local webserver daemons

--- custom/alias.zsh
+++ custom/alias.zsh
@@ -13,6 +13,9 @@ alias swap='sudo swapoff -a && sudo swapon -a'
 # Resource this file
 alias reload=". ~/.zshrc"
 
+# Start web server
+alias startwebserver="sudo /etc/rc.d/nginx start && sudo /etc/rc.d/php-fpm start && sudo /etc/rc.d/mongodb start && sudo /etc/rc.d/memcached start"
+
 # Restart web server
 alias restartwebserver="sudo /etc/rc.d/nginx restart && sudo /etc/rc.d/php-fpm restart && sudo /etc/rc.d/mongodb restart"
 
@@ -21,3 +24,12 @@ alias kc="eval `keychain --eval --agents ssh id_rsa`"
 
 # Man inside vim
 man() { vim -X -M -c "Man $*" -c "only" }
+
+# Calculator
+calc(){ awk "BEGIN{ print $* }" ;}
+
+# Switch to exercise.com hardware
+exercisehardware() {
+    xrandr --output VGA1 --auto
+    xrandr --output LVDS1 --off
+}
