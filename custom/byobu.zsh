efb4240a2900de59bfbd4aa48c29b5bec23162b6
ornicar <thibault.duplessis@gmail.com>
2011-01-20 09:24:33 -0800

    Rename byobu system presets

--- custom/byobu.zsh
+++ custom/byobu.zsh
@@ -1,6 +1,6 @@
 # Byobu presets
 alias blichess="cd ~/data/workspace/lichess && BYOBU_WINDOWS='li' byobu -S lichess"
 alias bexercise="cd ~/data/workspace/exercise && BYOBU_WINDOWS='ex' byobu -S exercise"
-alias bdotfiles="cd ~/data/workspace/dotfiles && BYOBU_WINDOWS='do' byobu -S dotfiles"
+alias bsystem="cd ~/data/workspace/dotfiles && BYOBU_WINDOWS='sy' byobu -S system"
 alias bim="BYOBU_WINDOWS='im' byobu -S im"
 alias bmedia="cd ~/data && BYOBU_WINDOWS='me' byobu -S media"
