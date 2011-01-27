9897e8783a53c70e6a99c1c6b3154bcafdae33cc
ornicar <thibault.duplessis@gmail.com>
2011-01-26 20:41:38 -0800

    Move custom configuration to monolythic .zshrc

--- custom/byobu.zsh
+++
@@ -1,6 +0,0 @@
-# Byobu presets
-alias blichess="cd ~/data/workspace/lichess && BYOBU_WINDOWS='li' byobu -S lichess"
-alias bexercise="cd ~/data/workspace/exercise && BYOBU_WINDOWS='ex' byobu -S exercise"
-alias bsystem="cd ~/data/workspace/dotfiles && BYOBU_WINDOWS='sy' byobu -S system"
-alias bim="BYOBU_WINDOWS='im' byobu -S im"
-alias bmedia="cd ~/data && BYOBU_WINDOWS='me' byobu -S media"
